10f1180b9a4e36a4cb17e52c7784b1ce9a2ae988
anastasiia <Anastasiia.Lepova@ivkhk.ee>
2020-11-03 13:35:15 +0200

    3 zadanie

--- src/startdew/StartDew.java
+++ src/startdew/StartDew.java
@@ -18,6 +18,23 @@ public class StartDew {
         Task1 task1 = new Task1();
         task1.run();
         
+        Task2 task2 = new Task2();
+        task2.run();
+        
+        Task3 task3 = new Task3();
+        task3.run();
+        
+        Task4 task4 = new Task4();
+        task4.run();
+        
+        Task5 task5 = new Task5();
+        task5.run();
+        
+        Task6 task6 = new Task6();
+        task6.run();
+        
+        Task7 task7 = new Task7();
+        task7.run();
     }
     
 }
